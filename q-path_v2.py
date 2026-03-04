1d1e4e95b814e74b249316d5674081fac8199aa1
sanantoniochili <to-tsi@windowslive.com>
2026-03-04 19:57:03 +0200

    Trying out networkx

--- q-path_v2.py
+++ q-path_v2.py
@@ -1,1 +1,49 @@
 import networkx as nx
+import numpy as np
+
+# cutoff fidelity 
+min_fidelity    = 0.8
+# randomly initialize the RNG from some platform-dependent source of entropy
+np.random.seed(0)
+# initialise graph
+G = nx.Graph()
+
+SDpairs = {
+    ### simple example with S-D and two repeaters ###
+    'simple': [
+        ('source','r1'), ('source', 'r2'), 
+        ('r1','r2'), ('r1','destination'), 
+        ('r2', 'destination'), 
+    ],
+
+    ### more complex example with S-D and 3 repeaters ###
+    'complex': [
+        ('source','r1'), ('source','r3'), 
+        ('r1','r2'), ('r1','r3'), 
+        ('r2','r3'), ('r2','destination'), 
+        ('r3','source'), ('r3','destination'),
+    ]
+}
+
+
+if __name__=="__main__":
+
+    E_plain = SDpairs['simple']
+
+    capacities = np.random.randint(low=5,high=10,size=len(E_plain))
+    fidelities = np.random.uniform(low=0.5, high=1, size=len(E_plain))
+
+    # assign capacities and fidelities
+    E_attributes = [
+        (edge[0], edge[1], {'weight': weight, 'fidelity': fidelity}) \
+            for (edge, weight, fidelity) in zip(E_plain,capacities,fidelities)
+    ]
+    # create graph
+    G.add_edges_from(E_attributes)
+    print(f"{'Edge':<22}{'Capacity':<12}{'Fidelity'}")
+    for edge in G.edges(data=True):
+        print(f"{f'({edge[0]},{edge[1]})':<22}{edge[2]['weight']:<12}{edge[2]['fidelity']}")
+
+
+
+
